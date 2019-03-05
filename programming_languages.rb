236f49ccce18b91f889865ae10c4c825ff6640a0
convivial-system-4430 <cdneal091@gmail.com>
2019-03-05 16:13:50 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do | style , langs |
     #binding.pry
-    lang.each do | lang, type|
+    langs.each do | lang, type|
       binding.pry
       new_hash[type][:type] = tip[:type]
       #binding.pry
