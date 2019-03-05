5c01727c5778773bd67432f47cdfa7d65cbd688b
convivial-system-4430 <cdneal091@gmail.com>
2019-03-05 16:40:45 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do | style , langs |
+    #binding.pry
+    langs.each do | lang, type|
+      #binding.pry
+      if !new_hash.key?(lang)
+        new_hash[lang] = {:type => (type[:type]), :style => []}
+      end
+      new_hash[lang][:style] << style
+      #binding.pry
+    end
+  end
+  #binding.pry
+  new_hash
 end
